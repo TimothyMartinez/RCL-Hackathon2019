ff10e5b61180d156526de87f8b2064c511597109
Timothy Martinez <tmrcmartinez@gmail.com>
2019-09-23 07:59:00 -0400

    im tiered

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,31 @@
 class WelcomeController < ApplicationController
+  before_action :set_user, only: [:show, :update, :edit]
 
+  def show
+  end
+
+  def update
+    
+  end
+
+  def edit
+    @users = current_user
+    @user = User.first.update(params[:id])
+    if @person.save
+      # success
+    else
+      # error handling
+    end
+  end
+
+  private 
+  
+  def set_user
+    @users = current_user
+  end 
+  
+  # def user_params 
+  #   params.require(:user).permit(:first, :second, :third, :fourth, :fifth)
+  # end 
   
 end
