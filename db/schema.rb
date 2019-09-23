ce935ef69df697ed46e7a57fd2586e9cf5945531
Anthony <agil013@fiu.edu>
2019-09-23 01:56:03 -0400

    working on navbar

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,16 @@
 
 ActiveRecord::Schema.define(version: 2019_09_23_024306) do
 
+  create_table "ideal_locations", force: :cascade do |t|
+    t.string "first"
+    t.string "second"
+    t.string "third"
+    t.string "fourth"
+    t.string "fifth"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
